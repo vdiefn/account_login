cbd7aa0d5ffb66f710596ae0cc489a84dd2a2109
vdiefn <ddhkxxhxx@hotmail.com>
2022-12-25 19:02:29 +0800

    feat:add detail page

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const mongoose = require('mongoose')
+const User = require('./models/user')
+
 const app = express()
 const port = 3000
 const db = mongoose.connection
@@ -11,9 +13,9 @@ if (process.env.NODE_ENV !== 'production') {
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true})
 
-
 app.engine('handlebars', exphbs({defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
+app.use(express.urlencoded({extended: true}))
 
 db.on('error', () => {
   console.log('mongodb error!')
@@ -26,6 +28,25 @@ db.once('open', () => {
 app.get('/', (req,res) =>{
   res.render('index')
 })
+
+app.post('/', (req, res) => {
+  console.log(req.body.email)
+  console.log(req.body.password)
+  const email = req.body.email
+  const password = req.body.password
+  return User.findOne({email:email})
+    .lean()
+    .then((user) => {
+      if(user !== null){
+        if(user.password === password)
+        res.render('detail', {user})
+      }else {
+        return res.redirect('/')
+      }
+    })   
+})
+
+
 
 app.listen( port, () => {
   console.log(`Express is running on http://localhost:${port}`)
