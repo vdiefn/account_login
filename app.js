caa06611087a59456fd284d3469e8201db2f642d
vdiefn <ddhkxxhxx@hotmail.com>
2022-12-25 15:56:14 +0800

    feat:add mongoose

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const mongoose = require('mongoose')
 const app = express()
 const port = 3000
+const db = mongoose.connection
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true})
+
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 app.get('/', (req,res) =>{
   res.render('index')
